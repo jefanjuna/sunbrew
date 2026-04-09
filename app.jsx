0e7e0e93e50a331a913ffec6a155e3853587e553
DevJoshCodes <devjoshcodes@gmail.com>
2026-04-09 15:51:13 +0800

    ch7 state interactions

--- app.jsx
+++ app.jsx
@@ -16,15 +16,44 @@
 
         const app = document.getElementById('app');
 
-        function Header() {
-            return <h1>Develop. Preview. Ship.</h1>
+        function createTitle(title) {
+            if (title) {
+                return title;
+            } else {
+                return 'Default title';
+            }
+        }
+
+        function Header({ title }) {
+            //console.log(props); // {title: 'React'}
+            console.log(title); // "React"
+            //return <h1>Develop. Preview. Ship.</h1>
+            //return <h1>{title}</h1>;
+            //return <h1>{`Cool ${title}`}</h1>;  // This is a grave accent ` or also known as backticks
+            //return <h1>{createTitle(title)}</h1>;
+            return <h1>{title ? title : 'Default Title'}</h1>; // This is a ternary operator
         }
 
         function HomePage() {
+            const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
             return (
             <div>
                 {/* Nesting the Header component */}
-                <Header />
+                <Header title="React" />
+                {/* This is example of prop usage and component reusing */}
+                <Header title="A new title" />
+                <ul>
+                    {/* You can then use the array.map() method to iterate over the array and use an arrow function to map a name to a list item: */}
+                    {/* {name.map((name) => (<li>{name}</li>))} */}
+                    {/*names.map((name) => (
+                        <li>{name}</li>
+                    ))*/}
+                    {names.map((name) => (
+                        <li key={name}>{name}</li>
+                    ))}
+                </ul>
+                <button onClick={}>Like</button>
             </div>
             );
         }
